877a1796319332d46962a3d4267d5815ea15d705
Daniel Esteban Gonzalez Zuluaga <danygonza961201@gmail.com>
2021-02-05 13:15:18 -0500

    Función de prestar libro

--- Biblioteca/Biblioteca.py
+++ Biblioteca/Biblioteca.py
@@ -10,14 +10,22 @@ class biblioteca:
                 self.libro[i]['Cantidad']+=cantidad
                 existe=True
         if existe == False:
-            print("hola")
             libro._cantidad=cantidad
             self.libro.append({"Nombre":libro.nombre,"Autor":libro.autor,"Genero":libro.genero,"Cantidad":libro._cantidad})
             print(self.libro[-1].values())
-    def Prestar_libro(self,libro):
-       # for i in libro:
-         #   if(libro.nombre)
-         pass
+    def Prestar_libro(self,libro,cantidad=1):
+        band=False
+        for i in range(len(self.libro)):
+            if(libro.nombre == self.libro[i].get('Nombre') and libro.autor == self.libro[i].get('Autor')):
+                band=True
+                if (self.libro[i].get('Cantidad')-cantidad)>=0:
+                    self.libro[i]['Cantidad']-=cantidad
+                    print("Se presto"+ str(cantidad))
+                else:
+                    print("No tenemos el o los libros que necesita")
+        if(band==False):
+            print("QQQNo tenemos el o los libros que necesita")
+                    
     def Vender_libros(self):
         pass 
     def Cuantos_libros(self):
