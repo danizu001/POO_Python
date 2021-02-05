fc36b8e602f25780a9071c6a9417f302871822bb
Daniel Esteban Gonzalez Zuluaga <danygonza961201@gmail.com>
2021-02-05 13:40:29 -0500

    Mejorando las funciones de vender y prestar

--- Biblioteca/Biblioteca.py
+++ Biblioteca/Biblioteca.py
@@ -1,3 +1,15 @@
+def Sacar_libro(prolibro,libro,cantidad):
+        band=False
+        for i in range(len(prolibro)):
+            if(libro.nombre == prolibro[i].get('Nombre') and libro.autor == prolibro[i].get('Autor')):
+                band=True
+                if (prolibro[i].get('Cantidad')-cantidad)>=0:
+                    prolibro[i]['Cantidad']-=cantidad
+                    print("Se prestaron: "+ str(cantidad)+" libros")
+                else:
+                    print("No tenemos el o los libros que necesita")
+        if(band==False):
+            print("No tenemos el o los libros que necesita")
 class biblioteca:
     def __init__(self,nombre):
         self.nombre=nombre
@@ -14,22 +26,12 @@ class biblioteca:
             self.libro.append({"Nombre":libro.nombre,"Autor":libro.autor,"Genero":libro.genero,"Cantidad":libro._cantidad})
             print(self.libro[-1].values())
     def Prestar_libro(self,libro,cantidad=1):
-        band=False
-        for i in range(len(self.libro)):
-            if(libro.nombre == self.libro[i].get('Nombre') and libro.autor == self.libro[i].get('Autor')):
-                band=True
-                if (self.libro[i].get('Cantidad')-cantidad)>=0:
-                    self.libro[i]['Cantidad']-=cantidad
-                    print("Se presto"+ str(cantidad))
-                else:
-                    print("No tenemos el o los libros que necesita")
-        if(band==False):
-            print("QQQNo tenemos el o los libros que necesita")
-                    
-    def Vender_libros(self):
-        pass 
+        Sacar_libro(self.libro,libro,cantidad)
+    def Vender_libros(self,libro,cantidad=1):
+        Sacar_libro(self.libro,libro,cantidad)
     def Cuantos_libros(self):
         pass
     def Buscar_libro(self):
         pass
     
+        
